2361f54ef883521fbf1166dfb9e771fad247d21a
ajkalb96 <43625012+ajkalb96@users.noreply.github.com>
2020-08-18 12:01:02 -0700

    CV!
    Created and added my CV, included it on the home page. Minor update to Hangman.

--- hangman.js
+++ hangman.js
@@ -4,7 +4,7 @@ var guesses;
 var plausible;
 resetGame();
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = [...Array(26)].reduce(a=>a+String.fromCharCode(i++),'',i=97).split("");
 alphabet.forEach(makeButton);
 
 function makeButton(item,index){
